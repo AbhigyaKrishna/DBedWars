af57e5e6113edac9d74e9156d57efd7aaa2e7a3b
RGoyalmc <76780190+RGoyalmc@users.noreply.github.com>
2021-11-16 11:26:09 +0530

    Update PopupTowerStructure.java

--- plugin/src/main/java/com/pepedevs/dbedwars/game/structure/PopupTowerStructure.java
+++ plugin/src/main/java/com/pepedevs/dbedwars/game/structure/PopupTowerStructure.java
@@ -14,6 +14,8 @@ public class PopupTowerStructure implements DirectionalStructure {
     private List<Block> eastBlocks;
     private List<Block> southBlocks;
 
+    private int blocksPerSecond;
+
     public PopupTowerStructure(){
         initNorthBlocks();
         initWestBlocks();
@@ -23,22 +25,22 @@ public class PopupTowerStructure implements DirectionalStructure {
 
     @Override
     public List<Block> getNorthBlocks() {
-        return null;
+        return new ArrayList<>(northBlocks);
     }
 
     @Override
     public List<Block> getWestBlocks() {
-        return null;
+        return new ArrayList<>(westBlocks);
     }
 
     @Override
     public List<Block> getEastBlocks() {
-        return null;
+        return new ArrayList<>(eastBlocks);
     }
 
     @Override
     public List<Block> getSouthBlocks() {
-        return null;
+        return new ArrayList<>(southBlocks);
     }
 
     @Override
@@ -48,42 +50,59 @@ public class PopupTowerStructure implements DirectionalStructure {
 
     @Override
     public void setNorthBlocks(List<Block> blocks) {
-
+        this.northBlocks = blocks;
     }
 
     @Override
     public void setWestBlocks(List<Block> blocks) {
-
+        this.westBlocks = blocks;
     }
 
     @Override
     public void setEastBlocks(List<Block> blocks) {
-
+        this.eastBlocks = blocks;
     }
 
     @Override
     public void setSouthBlocks(List<Block> blocks) {
-
+        this.southBlocks = blocks;
     }
 
     @Override
     public void setBlocks(BlockFace face, List<Block> blocks) {
-
+        switch (face){
+            case NORTH:
+                this.northBlocks = blocks;
+                return;
+            case EAST:
+                this.eastBlocks = blocks;
+                return;
+            case WEST:
+                this.westBlocks = blocks;
+                return;
+            case SOUTH:
+                this.southBlocks = blocks;
+        }
     }
 
     @Override
     public List<Block> getBlocks() {
-        return null;
+        return getNorthBlocks();
     }
 
     @Override
     public void setBlocks(List<Block> blocks) {
-
+        setNorthBlocks(blocks);
     }
 
     @Override
     public int getBlocksToPlacePerSecond() {
-        return 0;
+        return this.blocksPerSecond;
+    }
+
+    @Override
+    public void setBlocksToPlacePerSecond(int n) {
+        this.blocksPerSecond = n;
     }
 
     private void initNorthBlocks() {
