439068a87b8b6f9a169251ae50322cc96726f697
RGoyalmc <76780190+RGoyalmc@users.noreply.github.com>
2021-11-16 10:00:30 +0530

    Delete TeamStructure.java

--- api/src/main/java/com/pepedevs/dbedwars/api/game/struture/TeamStructure.java
+++
@@ -1,8 +0,0 @@
-package com.pepedevs.dbedwars.api.game.struture;
-
-import com.pepedevs.dbedwars.api.util.Color;
-
-public interface TeamStructure extends Structure {
-
-    Color getTeamColor();
-}
